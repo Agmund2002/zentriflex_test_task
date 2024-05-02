eaee6aae6aa28cb7245e6eb0066fd40f4a735da1
Andrii Malysh <andriiymalysh@gmail.com>
2024-05-02 21:23:12 +0300

    ci: add base file structure

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  try {
+    const app = await NestFactory.create(AppModule, { abortOnError: false })
+
+    app.setGlobalPrefix('api')
+    app.enableCors()
+    app.useGlobalPipes(new ValidationPipe())
+
+    await app.listen(process.env.PORT, () =>
+      console.log(`Server running. Use our API on port: ${process.env.PORT}`)
+    )
+  } catch (error) {
+    console.log(error.message)
+    process.exit(1)
+  }
 }
 bootstrap()
