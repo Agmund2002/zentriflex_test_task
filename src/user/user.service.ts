7f2660f192ac0c40adf557d220bac22712666f88
Andrii Malysh <andriiymalysh@gmail.com>
2024-05-02 23:38:37 +0300

    feat: add jwt strategy

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common'
+import { Prisma } from '@prisma/client'
+import { PrismaService } from 'src/prisma.service'
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  findBy(where: Prisma.UserWhereUniqueInput) {
+    return this.prisma.user.findUnique({ where })
+  }
+}
