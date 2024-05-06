b5d6080fb85726929b56712498f5b879c9e49123
Andrii Malysh <andriiymalysh@gmail.com>
2024-05-06 18:03:53 +0300

    feat: add is empty body middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
-import { Module } from '@nestjs/common'
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod
+} from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { AuthModule } from './auth/auth.module'
 import { UsersModule } from './users/users.module'
+import { IsEmptyBodyMiddleware } from './middlewares/is-empty-body.middleware'
 
 @Module({
   imports: [ConfigModule.forRoot(), AuthModule, UsersModule]
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(IsEmptyBodyMiddleware)
+      .forRoutes({ path: 'users/current', method: RequestMethod.PUT })
+  }
+}
